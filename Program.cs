bf66bfe719c7d35dceb414d8b0e685686b6f6c12
Falah Rafif <falah.rfif@gmail.com>
2021-10-18 15:09:53 +0700

    Add CRUD Operation in Program.cs

--- Program.cs
+++ Program.cs
@@ -20,8 +20,8 @@ namespace LearnNHibernate
             var cfg = new Configuration();
 
             #region Prepare Statment
-            String DataSource               =  @"UMMUTE\SQLEXPRESS";
-            String InitialCatalog           = "LearnNhibernate";
+            String DataSource               = @"DESKTOP-2UMMUTE\SQLEXPRESS";
+            String InitialCatalog           =  "LearnNhibernate";
             String IntegratedSecurity       =  "True";
             String ConnectTimeout           =  "15";
             String Encrypt                  =  "False";
@@ -32,7 +32,15 @@ namespace LearnNHibernate
 
             #region Set Up Connection String
             cfg.DataBaseIntegration(x => { 
-                x.ConnectionString = $"Data Source = {DataSource}; Initial Catalog = {InitialCatalog}; Integrated Security = {IntegratedSecurity}; Connect Timeout = {ConnectTimeout}; Encrypt = {Encrypt}; TrustServerCertificate = {TrustServerCertificate}; ApplicationIntent = {ApplicationIntent}; MultiSubnetFailover = {MultiSubnetFailover}"; 
+                x.ConnectionString = $@"
+                        Data Source = {DataSource}; 
+                        Initial Catalog = {InitialCatalog}; 
+                        Integrated Security = {IntegratedSecurity}; 
+                        Connect Timeout = {ConnectTimeout}; 
+                        Encrypt = {Encrypt}; 
+                        TrustServerCertificate = {TrustServerCertificate}; 
+                        ApplicationIntent = {ApplicationIntent}; 
+                        MultiSubnetFailover = {MultiSubnetFailover}"; 
 
                 x.Driver<SqlClientDriver>(); 
                 x.Dialect<MsSql2008Dialect>(); 
@@ -55,6 +63,123 @@ namespace LearnNHibernate
 
                 Console.ReadLine();
             }
+            #endregion
+            #endregion
+
+            #region CRUD Operation ////////////////////////////////////////////////////////////
+            #region Create
+            //using (var session = sefact.OpenSession())
+            //{
+
+            //    using (var tx = session.BeginTransaction())
+            //    {
+
+            //        var student1 = new Student
+            //        {
+            //            ID = 0,
+            //            FirstMidName = "Allan",
+            //            LastName = "Bommer"
+            //        };
+
+            //        var student2 = new Student
+            //        {
+            //            ID = 0,
+            //            FirstMidName = "Jerry",
+            //            LastName = "Lewis"
+            //        };
+
+            //        //save data
+            //        session.Save(student1);
+            //        session.Save(student2);
+            //        //Begin Transaction
+            //        tx.Commit();
+            //    }
+
+            //}
+            //Console.WriteLine("Create Operation Finish");
+            //Console.ReadLine();
+            #endregion
+
+            #region Read
+            using (var session = sefact.OpenSession())
+            {
+
+                using (var tx = session.BeginTransaction())
+                {
+                    var students = session.CreateCriteria<Student>().List<Student>();
+
+                    //Show All
+                    Console.WriteLine("List Students");
+                    foreach (var student in students)
+                    {
+                        Console.WriteLine("{0} \t{1} \t{2}",
+                           student.ID, student.FirstMidName, student.LastName);
+                    }
+
+                    //Show Specific Data
+                    var stdnt = session.Get<Student>(1);
+                    Console.WriteLine("Retrieved by ID");
+                    Console.WriteLine("{0} \t{1} \t{2}", stdnt.ID, stdnt.FirstMidName, stdnt.LastName);
+                    tx.Commit();
+                }
+
+                Console.ReadLine();
+            }
+            #endregion
+
+            #region Update
+            //using (var session = sefact.OpenSession())
+            //{
+
+            //    using (var tx = session.BeginTransaction())
+            //    {
+            //        var students = session.CreateCriteria<Student>().List<Student>();
+
+            //        var stdnt = session.Get<Student>(1);
+
+            //        Console.WriteLine("Update the last name of ID = {0}", stdnt.ID);
+            //        stdnt.LastName = "Donald";
+            //        session.Update(stdnt);
+            //        Console.WriteLine("\nFetch the complete list again\n");
+
+            //        foreach (var student in students)
+            //        {
+            //            Console.WriteLine("{0} \t{1} \t{2}", student.ID,
+            //               student.FirstMidName, student.LastName);
+            //        }
+
+            //        tx.Commit();
+            //    }
+
+            //    Console.ReadLine();
+            //}
+            #endregion
+
+            #region Delete
+            //using (var session = sefact.OpenSession())
+            //{
+
+            //    using (var tx = session.BeginTransaction())
+            //    {
+            //        var students = session.CreateCriteria<Student>().List<Student>();
+
+            //        var stdnt = session.Get<Student>(1);
+
+            //        Console.WriteLine("Delete the record which has ID = {0}", stdnt.ID);
+            //        session.Delete(stdnt);
+            //        Console.WriteLine("\nFetch the complete list again\n");
+
+            //        foreach (var student in students)
+            //        {
+            //            Console.WriteLine("{0} \t{1} \t{2}", student.ID, student.FirstMidName,
+            //               student.LastName);
+            //        }
+
+            //        tx.Commit();
+            //    }
+
+            //    Console.ReadLine();
+            //}
             #endregion
             #endregion
         }
