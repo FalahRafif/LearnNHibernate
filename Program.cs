ac2140b21d3f35134f1ce8d470e24ff96bad9350
Falah Rafif <falah.rfif@gmail.com>
2021-10-18 14:30:40 +0700

    add connection string in program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
 
 namespace LearnNHibernate
 {
@@ -10,6 +15,39 @@ namespace LearnNHibernate
     {
         static void Main(string[] args)
         {
+            //Data Source = DESKTOP - 2UMMUTE\SQLEXPRESS; Initial Catalog = LearnNhibernate; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False
+            var cfg = new Configuration();
+
+            String DataSource               =  @"UMMUTE\SQLEXPRESS";
+            String InitialCatalog           = "LearnNhibernate";
+            String IntegratedSecurity       =  "True";
+            String ConnectTimeout           =  "15";
+            String Encrypt                  =  "False";
+            String TrustServerCertificate   =  "False";
+            String ApplicationIntent        =  "ReadWrite";
+            String MultiSubnetFailover      =  "False";
+
+            cfg.DataBaseIntegration(x => { 
+                x.ConnectionString = $"Data Source = {DataSource}; Initial Catalog = {InitialCatalog}; Integrated Security = {IntegratedSecurity}; Connect Timeout = {ConnectTimeout}; Encrypt = {Encrypt}; TrustServerCertificate = {TrustServerCertificate}; ApplicationIntent = {ApplicationIntent}; MultiSubnetFailover = {MultiSubnetFailover}"; 
+                x.Driver<SqlClientDriver>(); 
+                x.Dialect<MsSql2008Dialect>(); 
+            });
+
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+
+            var sefact = cfg.BuildSessionFactory();
+
+            using (var session = sefact.OpenSession())
+            {
+
+                using (var tx = session.BeginTransaction())
+                {
+                    //perform database logic 
+                    tx.Commit();
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
