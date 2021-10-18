0b8acb0f51942b76f155efe1f294ee36bfc8ab18
Falah Rafif <falah.rfif@gmail.com>
2021-10-18 14:34:10 +0700

    update connection to db

--- Program.cs
+++ Program.cs
@@ -13,11 +13,13 @@ namespace LearnNHibernate
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            //Data Source = DESKTOP - 2UMMUTE\SQLEXPRESS; Initial Catalog = LearnNhibernate; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False
+            #region Connection To DB //////////////////////////////////////////////////////////
             var cfg = new Configuration();
 
+            #region Prepare Statment
             String DataSource               =  @"UMMUTE\SQLEXPRESS";
             String InitialCatalog           = "LearnNhibernate";
             String IntegratedSecurity       =  "True";
@@ -26,13 +28,18 @@ namespace LearnNHibernate
             String TrustServerCertificate   =  "False";
             String ApplicationIntent        =  "ReadWrite";
             String MultiSubnetFailover      =  "False";
+            #endregion
 
+            #region Set Up Connection String
             cfg.DataBaseIntegration(x => { 
                 x.ConnectionString = $"Data Source = {DataSource}; Initial Catalog = {InitialCatalog}; Integrated Security = {IntegratedSecurity}; Connect Timeout = {ConnectTimeout}; Encrypt = {Encrypt}; TrustServerCertificate = {TrustServerCertificate}; ApplicationIntent = {ApplicationIntent}; MultiSubnetFailover = {MultiSubnetFailover}"; 
+
                 x.Driver<SqlClientDriver>(); 
                 x.Dialect<MsSql2008Dialect>(); 
             });
+            #endregion
 
+            #region Connect To DB
             cfg.AddAssembly(Assembly.GetExecutingAssembly());
 
             var sefact = cfg.BuildSessionFactory();
@@ -48,6 +55,8 @@ namespace LearnNHibernate
 
                 Console.ReadLine();
             }
+            #endregion
+            #endregion
         }
     }
 }
